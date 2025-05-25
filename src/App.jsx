2fbc2f36be32e2a26d82626625f915d5811a1f2e
Popo <147984101+Popoisthebest@users.noreply.github.com>
2025-05-25 22:55:53 +0900

    pages/index-page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./component/DefaultLayout/Header/Header";
+import Footer from "./component/DefaultLayout/Footer/Footer";
+import MainPage from "./component/MainPage/MainPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
